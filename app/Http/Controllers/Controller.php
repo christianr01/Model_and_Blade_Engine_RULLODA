d3346ae27b1502d863d5f88134277f8de59ae296
Christian <christian.rulloda@gmail.com>
2024-04-16 16:11:41 +0800

    Creating Routes, Controllers and Views

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        $books = Book::all();
+        return view('books.index', compact('books'));
+    }
 }
